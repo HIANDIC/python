518a8b64a78d5ba7167c81b0d321dd67e8bac917
HIANDIC <leaands25@gmail.com>
2022-08-17 15:41:58 +0200

    adding explanation

--- sum_of_the_number.py
+++ sum_of_the_number.py
@@ -4,8 +4,10 @@
 number = int(input('enter a number: '))
 
 i = 1
+# initializing the sum variable
 sum = 0
 
+# creating a while loop for adding all numbers up to the given numbers to the sum
 while(i <= number):
     sum += i
     i += 1
