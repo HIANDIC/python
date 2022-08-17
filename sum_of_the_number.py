e9a05e8a978842a22a9a166956316536439107aa
HIANDIC <leaands25@gmail.com>
2022-08-17 15:46:01 +0200

    decrease the number of loops

--- sum_of_the_number.py
+++ sum_of_the_number.py
@@ -3,6 +3,7 @@
 # take a number from user
 number = int(input('enter a number: '))
 
+# we have begun with 1, because we want to decrease the loops number. Adding 0 to sum means nothing
 i = 1
 # initializing the sum variable
 sum = 0
@@ -11,4 +12,4 @@ sum = 0
 while(i <= number):
     sum += i
     i += 1
-print(f'sum is {sum}')
+print(f'sum of the numbers up to given number is : {sum}')
